519223b55217fcfa9d2e381a7f5fb3b36f8e3bed
Mohid Meer <mohidfauji@gmail.com>
2022-08-16 16:27:50 +0500

    Moved the Routes to grouped middleware Auth

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\FrontPostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GoogleController;
-use App\Http\Controllers\SocialMedia\DiscordController;
 use App\Http\Controllers\SocialMedia\FacebookController;
 use App\Http\Controllers\SocialMedia\InstagramController;
 use App\Http\Controllers\SocialMedia\TwitterController;
@@ -47,35 +46,6 @@ Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallba
 
 
 
-// Route::get('/home/discord', [DiscordController::class, 'index'])->name('discord');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/home/api', [FrontApiController::class, 'index'])->name('api');
-Route::get('/home/post', [FrontPostController::class, 'index'])->name('post');
-
-
-
-Route::get('home/facebook', [FacebookController::class, 'index'])->name('facebook');
-Route::get('home/facebook/deauth', [FacebookController::class, 'deauthorize'])->name('facebookdeauthorize');
-Route::get('home/facebook/deactivate/{id}', [FacebookController::class, 'deactivate'])->name('deactivate');
-Route::get('home/facebook/activate/{id}', [FacebookController::class, 'activate'])->name('activate');
-Route::get('home/facebook/login', [FacebookController::class, 'redirectToFacebook'])->name('facebooklogin');
-Route::get('home/facebook/callback', [FacebookController::class, 'handleFacebookCallback']);
-
-
-Route::get('/home/instagram', [InstagramController::class, 'index'])->name('instagram');
-Route::get('home/instagram/deauth', [InstagramController::class, 'deauthorize'])->name('instagramdeatuthorize');
-Route::get('home/instagram/deactivate/{id}', [InstagramController::class, 'deactivate'])->name('instagramdeactivate');
-Route::get('home/instagram/activate/{id}', [InstagramController::class, 'activate'])->name('instagramactivate');
-Route::get('home/instagram/login', [InstagramController::class, 'redirectToInstagram'])->name('instagramlogin');
-Route::get('home/instagram/callback', [InstagramController::class, 'handleInstagramCallback']);
-
-
-Route::get('/home/twitter', [TwitterController::class, 'index'])->name('twitter');
-Route::get('home/twitter/deauth', [twitterController::class, 'deauthorize'])->name('twitterdeatuthorize');
-Route::get('home/twitter/deactivate/{id}', [twitterController::class, 'deactivate'])->name('twitterdeactivate');
-Route::get('home/twitter/activate/{id}', [twitterController::class, 'activate'])->name('twitteractivate');
-Route::get('home/twitter/login', [twitterController::class, 'redirectTotwitter'])->name('twitterlogin');
-Route::get('home/twitter/callback', [twitterController::class, 'handleTwitterCallback']);
 
 
 
@@ -83,14 +53,33 @@ Route::get('home/twitter/callback', [twitterController::class, 'handleTwitterCal
 
 
 Route::middleware(['auth'])->group(function () {
-  
- 
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home/api', [FrontApiController::class, 'index'])->name('api');
+    Route::get('/home/post', [FrontPostController::class, 'index'])->name('post');
+
+
+
+    Route::get('home/facebook', [FacebookController::class, 'index'])->name('facebook');
+    Route::get('home/facebook/deauth', [FacebookController::class, 'deauthorize'])->name('facebookdeauthorize');
+    Route::get('home/facebook/deactivate/{id}', [FacebookController::class, 'deactivate'])->name('deactivate');
+    Route::get('home/facebook/activate/{id}', [FacebookController::class, 'activate'])->name('activate');
+    Route::get('home/facebook/login', [FacebookController::class, 'redirectToFacebook'])->name('facebooklogin');
+    Route::get('home/facebook/callback', [FacebookController::class, 'handleFacebookCallback']);
+
+
+    Route::get('/home/instagram', [InstagramController::class, 'index'])->name('instagram');
+    Route::get('home/instagram/deauth', [InstagramController::class, 'deauthorize'])->name('instagramdeatuthorize');
+    Route::get('home/instagram/deactivate/{id}', [InstagramController::class, 'deactivate'])->name('instagramdeactivate');
+    Route::get('home/instagram/activate/{id}', [InstagramController::class, 'activate'])->name('instagramactivate');
+    Route::get('home/instagram/login', [InstagramController::class, 'redirectToInstagram'])->name('instagramlogin');
+    Route::get('home/instagram/callback', [InstagramController::class, 'handleInstagramCallback']);
+
+
+    Route::get('/home/twitter', [TwitterController::class, 'index'])->name('twitter');
+    Route::get('home/twitter/deauth', [twitterController::class, 'deauthorize'])->name('twitterdeatuthorize');
+    Route::get('home/twitter/deactivate/{id}', [twitterController::class, 'deactivate'])->name('twitterdeactivate');
+    Route::get('home/twitter/activate/{id}', [twitterController::class, 'activate'])->name('twitteractivate');
+    Route::get('home/twitter/login', [twitterController::class, 'redirectTotwitter'])->name('twitterlogin');
+    Route::get('home/twitter/callback', [twitterController::class, 'handleTwitterCallback']);
 });
-
-
-
-
-
-
-
-
