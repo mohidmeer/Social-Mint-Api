3b00c1b80f3c68b13ad321c7e564e7edb0a54c94
Mohid Meer <mohidfauji@gmail.com>
2022-08-23 03:49:18 +0500

    modified page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -12,7 +12,7 @@
 <article>
     <h1>Under Development</h1>
     <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:mohidfauji@mmm#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment.We&rsquo;ll be back online shortly!</p>
+        <p>&mdash;Social mint share</p>
     </div>
 </article>
