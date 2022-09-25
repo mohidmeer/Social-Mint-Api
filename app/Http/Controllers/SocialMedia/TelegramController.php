daf43752dd1477273af902277fa01df96c844e36
Mohid Meer <mohidfauji@gmail.com>
2022-09-26 01:23:33 +0500

    Added Logic For Saving Telegram

--- app/Http/Controllers/SocialMedia/TelegramController.php
+++ app/Http/Controllers/SocialMedia/TelegramController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\SocialMedia;
 
 use App\Http\Controllers\Controller;
+use App\Models\Telegram\Telegram;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TelegramController extends Controller
 {
@@ -18,6 +20,18 @@ class TelegramController extends Controller
     }
 
     public function save(Request $request){
+
+        $request->validate([ 
+            'name' => 'required|min:4']);
+
+
+        Telegram::create([
+            'user_id'=>Auth::user()->id,
+            'name'=>$request->name
+
+        ]);
+
+        return redirect()->route('telegram')->with('success','Telegram Name Added Make Sure to Send Test Message');
 
 
 
