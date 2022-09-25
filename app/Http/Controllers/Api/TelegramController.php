f765f2cd19401865cbdc7bbc68bd3132e44a0ea9
Mohid Meer <mohidfauji@gmail.com>
2022-09-26 01:48:33 +0500

    Added Post message APi Logic for telegram

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class TelegramController extends Controller
 {
@@ -11,15 +13,25 @@ class TelegramController extends Controller
 
 
     public function PostMessage(Request $request){
+        $request->validate([ 
+            'message'=>'required'
+        ]);
 
+        if (!(isset(Auth::user()->Telegram))){ return response('No Account Linked',404); }
 
+        $chat_id= Auth::user()->Telegram->name;
 
+        $url='https://api.telegram.org/bot'
+        .config('serices.telegram.key').'/sendMessage?chat_id='.$chat_id.'&text='.$request->message.'';
+        $sendMessageTelegram=Http::get($url);
+
+        return response($sendMessageTelegram,200);
 
     }
 
     public function PostMedia(Request $request){
 
-        
+
 
 
 
