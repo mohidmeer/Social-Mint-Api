8c1416d1ef460a6e7b5d9de9940e482ad1135b61
Mohid Meer <mohidfauji@gmail.com>
2022-10-07 00:58:03 +0500

    Added Api Endpoints for Pintrest

---
+++ app/Http/Controllers/Api/PintrestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PintrestController extends Controller
+{
+
+    public function Pin(Request $request)
+    {
+
+        
+
+    }
+    
+
+
+
+
+}
--- routes/SocialMediaApi.php
+++ routes/SocialMediaApi.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Api\TwitterController;
 use App\Http\Controllers\Api\RedditController;
 use App\Http\Controllers\Api\TelegramController;
 use App\Http\Controllers\Api\DiscordController;
+use App\Http\Controllers\Api\PintrestController;
 use App\Http\Requests\PostFacebookRequest;
+use App\Models\Pintrest\Pintrest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
   
@@ -15,28 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('facebook/pic',[FacebookController::class, 'postPic']);
-    
     Route::post('facebook/feed',[FacebookController::class, 'postFeed']);
 
     Route::post('instagram/pic',[InstagramController::class, 'postPic']);
 
     Route::post('twitter/tweet',[TwitterController::class, 'PostTweet']);
-    
     Route::post('twitter/tweetMedia',[TwitterController::class, 'PostMediaTweet']);
 
     Route::post('reddit/feed',[RedditController::class, 'Postmessage']);
-    
     Route::post('reddit/pic',[RedditController::class, 'PostMedia']);
 
     Route::post('telegram/feed',[TelegramController::class, 'PostMessage']);
-    
     Route::post('telegram/pic',[TelegramController::class, 'PostMedia']);
 
-
     Route::post('discord/feed',[DiscordController::class, 'PostMessage']);
-
-    
     Route::post('discord/pic',[DiscordController::class, 'PostMedia']);
+
+    Route::post('pintrest/pin',[PintrestController::class,'Pin']);
 
     
     
